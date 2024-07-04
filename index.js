6060326bc0170b42490745dca0fa00431428fe87
muzamil ahmed <amuzammil707@gmail.com>
2024-07-04 16:35:49 +0500

    Adding a Number Guessing Game

---
+++ index.js
@@ -0,0 +1,18 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+console.log("Wellcome Muzamil Ahmed cli guessing number game");
+const randomNmuber = Math.floor(Math.random() * 5 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "Userguessednumber",
+        type: "number",
+        messag: "Enter your guess Number(Number limit 5 to 1):",
+    },
+]);
+if (answer.Userguessednumber === randomNmuber) {
+    console.log("Congratulation ! You guess a correct Number..");
+}
+else {
+    console.log("sorry, You guess a wrong number");
+}
+;
